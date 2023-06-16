8ce25f4e459ddae0d5dd98d27fc60ed13ee3f563
Atlungstad Brenneri <56583550+camilla-design@users.noreply.github.com>
2023-06-16 19:15:32 +0200

    fixing error

--- utils/fetchFeedbacks.ts
+++ utils/fetchFeedbacks.ts
@@ -1,4 +1,4 @@
-import { FeedbackInformation } from "@/typings";
+import { FeedbackInformation } from "../typings";
 
 export const fetchFeedbacks = async() => {
     const res = await fetch(`${process.env.NEXT_PUBLIC_BASE_URL}/api/getFeedbackInfo`);
