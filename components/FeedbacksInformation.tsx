48da7db68fc5ba7cc17c035ba75e6c321e6ddb1f
Atlungstad Brenneri <56583550+camilla-design@users.noreply.github.com>
2023-06-17 10:45:19 +0200

    done

--- components/FeedbacksInformation.tsx
+++ components/FeedbacksInformation.tsx
@@ -1,40 +1,36 @@
-import React from 'react';
+import React from "react";
 import { motion } from "framer-motion";
-import FeedbackCardAtlungstad from './FeedbackCardAtlungstad';
-import FeedbackCardExplore from './FeedbackCardExplore';
-import FeedbackCardNoroff from './FeedbackCardNoroff';
+import FeedbackCardAtlungstad from "./FeedbackCardAtlungstad";
+import FeedbackCardExplore from "./FeedbackCardExplore";
+import FeedbackCardNoroff from "./FeedbackCardNoroff";
 
+type Props = {};
 
-type Props = {
-    
-}
-
-function Feedback({  }: Props) {
+function Feedback({}: Props) {
   return (
-    <motion.div 
-    initial={{
+    <motion.div
+      initial={{
         opacity: 0,
-    }}
-    whileInView={{
+      }}
+      whileInView={{
         opacity: 1,
-    }}
-    transition={{
+      }}
+      transition={{
         duration: 1.4,
-    }}
+      }}
+      className="h-screen flex relative overflow-hidden flex-col text-lft md:flex-row max-w-full px-10 justify-evenly mx-auto items-center"
+    >
+      <h3 className="absolute top-10 uppercase tracking-[15px] text-gray-400 pb-2 text-sm">
+        Omtale
+      </h3>
 
-    className='h-screen flex relative overflow-hidden flex-col text-lft md:flex-row max-w-full px-10 justify-evenly mx-auto items-center'>
-        <h3 className='absolute top-10 uppercase tracking-[15px] text-gray-400 pb-2 text-sm'>Omtale</h3>
-
-        <div className='w-full flex space-x-5 overflow-x-scroll p-10 snap-x snap-mandatory scrollbar scrollbar-track-gray-400/20 scrollbar-thumb-[#8C4D16]'>
-            
-          
-            <FeedbackCardAtlungstad  />
-            <FeedbackCardExplore />
-            <FeedbackCardNoroff />
-          
-        </div>
+      <div className="w-full flex space-x-5 overflow-x-scroll p-10 snap-x snap-mandatory scrollbar scrollbar-track-gray-400/20 scrollbar-thumb-[#8C4D16]">
+        <FeedbackCardAtlungstad />
+        <FeedbackCardExplore />
+        <FeedbackCardNoroff />
+      </div>
     </motion.div>
-  )
+  );
 }
 
-export default Feedback
+export default Feedback;
